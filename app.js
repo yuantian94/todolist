c5e5f60712d9fb69eede62fe1a6c0b8b0a704026
Yuan-Tian <Yuan Tian>
2022-10-27 02:36:44 -0500

    Add MongoDB and Multiple-list Functionality

--- app.js
+++ app.js
@@ -2,35 +2,115 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname+"/date.js");
 const app = express();
+const mongoose = require("mongoose");
+const _= require("lodash");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-let items = [];
+
 let workItems =[];
 
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser:true});
+
+const itemSchema = new mongoose.Schema({
+  name:String
+});
+
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+}
+
+
+const Item = new mongoose.model("Item", itemSchema);
+
+const List = new mongoose.model("List",listSchema);
+
 app.get("/",(req,res)=>{
-  let page ="/";
   let day = date.getDate();
-  res.render("list", {listTitle: day, newListItems:items, pagePostedFrom:page});
+  let items = Item.find({},(err,foundItems)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.render("list", {listTitle: day, newListItems:foundItems});
+    }
+  });
+
+});
+
+app.get("/:customListName", (req, res)=>{
+  const customListName = _.capitalize(req.params.customListName);
+  List.findOne({name:customListName}, (err,results)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(results){
+        res.render("list", {listTitle: customListName, newListItems:results.items});
+      }
+      else{
+        const list = new List({name:customListName, items:[]});
+        list.save();
+        res.redirect("/"+customListName);
+      }
+    }
+  });
+
+
 });
 
 app.post("/",(req,res)=>{
-  let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  const item = req.body.newItem;
+  const listName = req.body.list;
+
+  const itemInsert = new Item({name:item});
+
+  if(listName === date.getDate()){
+    itemInsert.save();
+    res.redirect("/");
+  }
+  else{
+    List.findOne({name:listName}, (err,results)=>{
+      results.items.push(itemInsert);
+      results.save();
+      res.redirect("/"+listName);
+    });
+  }
+
+
 });
 
+app.post("/delete",(req,res)=>{
+  const checkedItemID = req.body.checkbox;
+  const listName = req.body.listName;
 
-app.get("/work", (req,res)=>{
-  let page ="work";
-  res.render("list",{listTitle: "Work List", newListItems: workItems, pagePostedFrom:page});
+  if(listName === date.getDate()){
+    Item.deleteOne({_id:checkedItemID},(err)=>{
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log("Succuessful deletion");
+      }
+    });
+    res.redirect("/");
+  }
+  else{
+    List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemID}}},(err, results)=>{
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.redirect("/"+listName);
+      }
+    });
+  }
+
+
 });
 
-app.post("/work", (req,res)=>{
-  let item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
-})
 
 app.get("/about", (req,res)=>{
   res.render("about");
